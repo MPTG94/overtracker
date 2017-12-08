591071f10e41617ce2a32a8bde9bc8792cd36ce7
Mor Paz <1912.mor@gmail.com>
2017-12-08 20:51:47 +0200

    Add server logic, begin work on fetch logic on client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function callAPI(query, callback) {
+  console.log('test');
+  return fetch('api/test', {
+    accept: 'application/json'
+  }).then(parseJSON);
+}
+
+function parseJSON(response) {
+  console.log('Got response!');
+  return response.json();
+}
+
+console.log('testing!');
+const result = callAPI();
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +24,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!</h1>
+          <h2>{JSON.stringify(result)}LL</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload Test!
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello!' });
+});
+
+// Setting app listen on port 3001
+app.listen(3001, function() {
+  console.log('Example app listening on port 3001!');
+});
